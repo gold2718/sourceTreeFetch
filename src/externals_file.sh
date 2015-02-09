f3b52fbea5d11aa718b77d035ceb929c77a8f76e
Steve Goldhaber <goldy@ucar.edu>
2015-02-09 16:33:49 -0700

    Bash-based prototype checkout tool for git-based NorESM Can parse and print an externals file

---
+++ src/externals_file.sh
@@ -0,0 +1,170 @@
+#! /bin/bash
+
+## Parse an Externals.cfg file into an internal format and provide routines
+## to provide useful information as requested.
+
+if [ -f "/utils.sh" ]; then
+    . utils.sh
+elif [ -f "src/utils.sh" ]; then
+    . src/utils.sh
+else
+    echo "Cannot find utils.sh"
+    exit 1
+fi
+
+## What are the allowed keywords in an Externals.cfg section?
+declare -A EXTERNAL_KEYWORDS=(["branch"]=1 ["externals"]=1 ["from_submodule"]=1 \
+                              ["hash"]=1 ["local_path"]=1 ["protocol"]=1        \
+                              ["repo_url"]=1 ["required"]=1 ["sparse"]=1 ["tag"]=1)
+
+CFG_DESC_NAME="externals_description"
+VERSION_KEY="schema_version"
+CFG_FILENAME_KEY="xxCfgFilePath"
+SECTION_REGEX='\[[[:space:]]*([-A-Za-z0-9_]+)[[:space:]]*]'
+
+cfgerr() {
+    ## On an error condition ($1 != 0), print an error message and quit
+    ## $1 is the status (zero or an error code)
+    ## $2 is the current config line number
+    ## $3 is the current config filename
+    ## $4 is an error message
+    if [ ${1} -ne 0 ]; then
+        echo "ERROR: ${4} on ${3}:${2}"
+    fi
+    return ${1}
+}
+
+cfg_to_externals() {
+    ## Convert an internal configuration format $2 to externals
+    ## Add each external to the input $1.
+    local sections=(${2//${SECTION_CHR}/ })
+    local val
+    local version
+
+    for section in ${sections[@]}; do
+        tmparr=(${section//${NAME_CHR}/ })
+        eval "${1}[\${tmparr[0]}]=\${tmparr[1]}"
+    done
+}
+
+parse_externals_cfg_file() {
+    ## Given a file, parse an externals file into an internal format
+    local config=""       # The parsed configuration
+    local current_ext=""  # The name of the current external being parsed
+    local -A externals=() # For collecting externals during parsing
+    local inline          # Current line minus leading and trailing whitespace
+    local key
+    local line            # Current line being parsed
+    local lineno=0        # Current line number
+    local res
+    local tmp
+    local value
+    local version=""      # The configuration file schema version
+
+    while read line; do
+        lineno=$((lineno + 1))
+        inline="$(echo ${line} | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')"
+        inline="$(strip_arg ${line})"
+        if [ ${#inline} -eq 0 ]; then
+            # Blank line, ignore
+            continue
+        elif [ "${inline:0:1}" == "#" ]; then
+            # Comment, ignore
+            continue
+        elif [[ "${inline}" =~ ${SECTION_REGEX} ]]; then
+            current_ext="${BASH_REMATCH[1]}"
+            valid_string "${current_ext}" "${SPECIAL_CHRS}"
+            res=$?
+            if [ ${res} -ne 0 ]; then
+                cfgerr ${CFG_INTERNAL_ERR} ${lineno} ${1} \
+                       "Invalid character in external name, '${current_ext}'"
+            fi
+            if [[ -v externals[${current_ext}] ]]; then
+                cfgerr ${CFG_EXTNAME_ERROR} ${lineno} ${1} \
+                       "Duplicate external name, ${current_ext}"
+            elif [ "${current_ext}" != "${CFG_DESC_NAME}" ]; then
+                externals[${current_ext}]=""
+            fi # No else, special case handled in keyword section
+        elif [ -n "$(echo ${inline} | grep '=')" ]; then
+            key="$(parse_keyword ${inline})"
+            val="$(parse_value ${inline})"
+            if [ -z "${current_ext}" ]; then
+                cfgerr ${CFG_SYNTAX_ERROR} ${lineno} ${1} \
+                       "Invalid keyword line, not parsing a section"
+            elif [ "${current_ext}" == "${CFG_DESC_NAME}" ]; then
+                if [ "${key}" == "${VERSION_KEY}" ]; then
+                    valid_string "${val}" "${SPECIAL_CHRS}"
+                    res=$?
+                    if [ ${res} -eq 0 ]; then
+                        version="${val}"
+                    else
+                        cfgerr ${CFG_INTERNAL_ERR} ${lineno} ${1} \
+                               "Invalid character in '${val}'"
+                    fi
+                else
+                    cfgerr ${CFG_KEYWORD_ERROR} ${lineno} ${1} \
+                           "Invalid keyword, '${key}', in ${CFG_DESC_NAME} section"
+                fi
+            elif [[ -v EXTERNAL_KEYWORDS[${key}] ]]; then
+                valid_string "${val}" "${SPECIAL_CHRS}"
+                res=$?
+                if [ ${res} -eq 0 ]; then
+                    tmp="${key}${KEYVAL_SEP}${val}"
+                    if [ -n "${externals[${current_ext}]}" ]; then
+                        tmp="${externals[${current_ext}]}${KEYVAL_CHR}${tmp}"
+                    fi
+                    externals[${current_ext}]="${tmp}"
+                else
+                    cfgerr ${CFG_INTERNAL_ERR} ${lineno} ${1} \
+                           "Invalid character in '${val}'"
+                fi
+            else
+                cfgerr ${CFG_KEYWORD_ERROR} ${lineno} ${1} \
+                       "Invalid keyword, '${key}', in ${current_ext} section"
+            fi
+        else
+            cfgerr ${CFG_SYNTAX_ERROR} ${lineno} ${1} "Syntax error"
+        fi
+    done < "${1}"
+    # Wrapup
+    config="${CFG_DESC_NAME}${NAME_CHR}${CFG_FILENAME_KEY}${KEYVAL_SEP}${1}"
+    config="${config}${KEYVAL_CHR}${VERSION_KEY}${KEYVAL_SEP}${version}"
+    line="$(echo ${!externals[@]} | sort)"
+    for key in ${line}; do
+        config="${config}${SECTION_CHR}${key}${NAME_CHR}${externals[${key}]}"
+    done
+    echo "${config}"
+}
+
+print_externals_cfg() {
+    ## Pretty print an externals configuration ($1)
+    local -A externals=()
+    local file
+    local key
+    local keyval
+    local section
+    local sections
+    local val
+    local version
+
+    cfg_to_externals externals "${1}"
+    for section in $(echo ${!externals[@]} | tr ' ' '\n' | sort); do
+        if [ "${section}" != "${CFG_DESC_NAME}" ]; then
+            echo "[${section}]"
+        fi
+        for keyval in ${externals[${section}]//${KEYVAL_CHR}/ }; do
+            key=$(echo ${keyval} | cut -d"${KEYVAL_SEP}" -f1)
+            val=$(echo ${keyval} | cut -d"${KEYVAL_SEP}" -f2-)
+            if [ "${section}" == "${CFG_DESC_NAME}" ]; then
+                if [ "${key}" == "${VERSION_KEY}" ]; then
+                    version="${val}"
+                fi
+            else
+                echo "  ${key} = ${val}"
+            fi
+        done
+        echo ""
+    done
+    echo "[${CFG_DESC_NAME}]"
+    echo "  ${VERSION_KEY} = ${version}"
+}
---
+++ src/utils.sh
@@ -0,0 +1,76 @@
+## Error codes
+CFG_INTERNAL_ERR=11
+CFG_SYNTAX_ERROR=12
+CFG_EXTNAME_ERROR=13
+CFG_KEYWORD_ERROR=14
+CFG_INVALID_CHAR=15
+
+## Special separator characters, not allowed in configuration file
+## While this could technically show up in a directory name, does anyone use it?
+SECTION_CHR='&'
+KEYVAL_CHR='@'
+KEYVAL_SEP=':' # Allowed in values so not included in SPECIAL_CHRS
+NAME_CHR=';'
+SPECIAL_CHRS="${SECTION_CHR}|${NAME_CHR}|${KEYVAL_CHR}"
+
+## Utility functions
+
+tbl_gen_hash_key=(61 59 53 47 43 41 37 31 29 23 17 13 11 7 3 1)
+tbl_gen_len=${#tbl_gen_hash_key}
+gen_hash_key_offset=21467
+
+file_hash() {
+    ## Given a full pathname to a file, create a uniqe hash
+    local chr
+    local chrs
+    local hash
+    local -i hind
+    local -i hval
+    local -i val
+    if [ -n "$(which xshasum)" ]; then
+        hash=$(echo "${1}" | shasum | cut -d' ' -f1)
+    else
+        hval=${gen_hash_key_offset}
+        hind=0
+        chrs=($(echo "${1}" | xxd -c 1 | cut -d' ' -f2))
+        for chr in ${chrs[@]}; do
+            val=$(printf %d "0x${chr}")
+            hval=$((hval ^ (val * tbl_gen_hash_key[${hind}])))
+            hind=$((hind + 1))
+            if [ ${hind} -ge ${tbl_gen_len} ]; then
+                hind=0
+            fi
+        done
+        hash="$(printf %x ${hval})"
+    fi
+    echo "${hash}"
+}
+
+strip_arg() {
+    ## Simply strip the leading and trailing whitespace around the
+    ## input arguments. Note, multiple internal spaces converted to a
+    ## single space
+    echo $@
+}
+
+parse_keyword() {
+    ## Parse a line that looks like 'keyword = value' and return the keyword
+    local key="$(strip_arg $(echo ${@} | cut -d'=' -f1))"
+    echo "${key}"
+}
+
+parse_value() {
+    ## Parse a line that looks like 'keyword = value' and return the value
+    local val="$(strip_arg $(echo ${@} | cut -s -d'=' -f2))"
+    echo "${val}"
+}
+
+valid_string() {
+    ## Check to see if string ($1) has any invalid character
+    ## $2 is a regex to detect any invalid character or sequence
+    if [[ "${1}" =~ ${2} ]]; then
+        return ${CFG_INVALID_CHAR}
+    else
+        return 0
+    fi
+}
